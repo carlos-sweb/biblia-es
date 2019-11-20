df438a6629622ee70f65dcbaa644a9aa849f43bb
carlos-sweb <c4rl0sill3sc4>
2019-11-20 19:58:59 -0300

    update

--- js/main.js
+++ js/main.js
@@ -28,16 +28,42 @@ require.config({
 
 require(["init","zepto"],
 function(init,$){
-
-
+	
+ 
+	 var HeaderBiblia = {
+	 	  view:function(vnode){
+	 	  	 let libro= vnode.attrs.libro;
+	 	  	   return [
+	 	  	  			 m("div.divider"),
+	 	  	  			 m("div",{class:"row start-sm center-xs animated fadeIn"},[
+	 	  	  			   m("div.col-xs-11",[
+	 	  	  			   		m("div.form-group",[
+	 	  	  			   			m("select.form-select",{
+	 	  	  			   					id:"capitulo",
+	 	  	  			   					onchange:function(event){
+	 	  	  			   				    m.route.set("/:libro/1",{libro:event.target.value})
+	 	  	  			   			  }},
+	 	  	  			   			
+	 	  	  			   			 _.map(init.libros,(l)=>{   
+      return m("option",
+         (libro == l.val ? { value:l.val,selected:true } : { value:l.val }),l.text);
+      })
+	 	  	  			   			
+	 	  	  			   			)
+	 	  	  			   		])
+	 	  	  			   ])
+	 	  	  			 ]),
+	 	  	  		m("div.divider")
+	 	  	   ];
+	 	  }
+	 }
+	 
   var Data = {
-      content:[],
       fetch:function(libro){
          return  m.request({
                   method: "GET",
                   url: "json/"+libro+".json"
-            })
-            
+            })   
       }
   }
 
@@ -53,11 +79,11 @@ function(init,$){
         },
         view: function(vnode){
             const libro = vnode.attrs.libro || "genesis" ;
+            $("#libro").val(libro)
             const capitulo = vnode.attrs.capitulo || "1" ;
             if( this._content != null ){
               if( this.libro != libro ){
-                      //alert("no render");
-                      
+                      //alert("no render");   
                       Data.fetch(vnode.attrs.libro).then((result)=>{
                         this._content = result;
                         this.libro = libro;
@@ -65,9 +91,13 @@ function(init,$){
                       return m("div",{class:"loading loading-lg"})
               }else{
                     //alert("render");
-                    return _.map(this._content[capitulo],function(cap,iterator){      
-                          return [m("div",{class:"col-xs-10 col-xs-offset-1"},[m("strong",iterator),m("p",cap)]),m("div",{class:"divider"})]
-                    });
+                  /* const c = _.map(this._content[capitulo],function(cap,iterator){      
+                        return [m("div",{class:"col-xs-10 col-xs-offset-1"},[m("strong",iterator),m("p",cap)]),m("div",{class:"divider"})]
+                   });*/
+                   
+                   return [m(HeaderBiblia,{libro:libro}),_.map(this._content[capitulo],function(cap,iterator){      
+                        return [m("div",{class:"col-xs-10 col-xs-offset-1"},[m("strong",iterator),m("p",cap)]),m("div",{class:"divider"})]
+                   })]
               };
               
             }else{
@@ -82,26 +112,21 @@ function(init,$){
     }
 
     
-   $(init.e_libros).on("change",function(){
-      m.route.set( "/:libro/:capitulos" , { capitulos: $("#capitulo").val(), libro: $(this).val() } )
-   });
+
  
  /*cambio de capitulo*/
- $("#capitulo").on("change",
- function(){
- 	   m.route.set( "/:libro/:capitulos" , { capitulos: $(this).val(), libro: $(init.e_libros).val() } )
- })
+ 
    
   /*renderizando*/
-  m.render(
+/* m.render(
    init.e_libros,
    _.map(init.libros,(l)=>{   
       return m("option",
       { value:l.val },l.text);
-   }))
+   }))*/
   /*renderizando*/
   /*Router para la biblia*/
-  m.route(document.querySelector("#content"),
+  m.route(document.body,
       "/genesis/1",{
       "/:libro/:capitulo": Biblia
   })
